9cc336f4e93516a06611c9c6490b14a0fb0f7f9d
JaniceNova <janicenovacek2016@gmail.com>
2019-01-25 22:45:16 -0800

    banner bar sizing correctly

---
+++ src/components/Banner/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './banner.css';
+
+class Banner extends Component {
+  render() {
+    return (
+      <div id="Banner">
+      <div id="words"><div id="orangeWord">Super </div> <div id="blueWord">Schedular </div></div>
+      
+      </div>
+    );
+  }
+}
+
+export default Banner;
